2d37ca7dbe91f708a69597a8faeb2fcbdc49b5b9
Benedettopana <benedetto.pana@gmail.com>
2024-04-15 16:57:38 +0200

    [add]Bonus 1

--- index.php
+++ index.php
@@ -40,7 +40,31 @@ $hotels = [
 
 ];
 
-// var_dump($hotels);
+$preferenza = $_POST;
+$parking;
+$newHotel = [];
+// SCELTA
+if(isset($_POST['preferenza']) && !empty($_POST['preferenza'])){
+  foreach($hotels as $element){
+    if( $_POST['preferenza'] == '1'){
+      if($element['parking']){
+        array_push($newHotel, $element);
+      }
+      
+    }else if($_POST['preferenza'] == '2'){
+      if(!$element['parking'])
+      array_push($newHotel, $element);
+    }else{
+      array_push($newHotel, $element);
+    }
+  }
+  
+// var_dump($newHotel);
+
+}
+
+// /SCELTA
+// var_dump($parking);
 ?>
 
 <!DOCTYPE html>
@@ -51,29 +75,41 @@ $hotels = [
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css' integrity='sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg==' crossorigin='anonymous'/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- link css -->
-  <link rel="stylesheet" href="./css/style.css">
-  <style>
-    a{
-      --bs-link-color-rgb: white !important;
-      text-decoration: none !important;
-    }
-  </style>
+  <link rel="stylesheet" href="./style.css">
   <title>PHP Hotel</title>
 </head>
 <body>
   <div class="container my-5">
-    <div class="row row-cols-2 ">
-    <?php 
-      foreach ($hotels as $elements) :
-      ?>
-      <div class="col my-3 ">
-          <?php foreach ($elements as $key => $item ) : ?>
-          <h4><?php echo $key ?>: <?php echo $item ?></h4>
-          <?php endforeach ?>
+    <!-- Select parcheggio -->
+    <div class=" my-5">
+      <form action="index.php" method="POST">
+        <select class="form-select" aria-label="Default select example" name="preferenza">
+          <option selected>Preferenza parcheggio</option>
+          <option value="1">Presente</option>
+          <option value="2">Senza</option>
+          <option value="3">Indifferente</option>
+        </select>
+        <!-- BTN -->
+        <div class="my-3">
+          <button type="submit" class="btn btn-primary">Cerca</button>
+          <button type="reset" class="btn btn-danger">Reset</button>
         </div>
+      </form>
+    </div>
+    <?php if(!empty($_POST['preferenza'])): ?>
+    <div class="row row-cols-2  justify-content-center ">
+    <?php 
+      foreach ($newHotel as $elements) :
+      ?>
+          <div class="col-5 my-3  hotel ">
+            <?php foreach ($elements as $key => $item ) : ?>
+              <h4><?php echo $key ?>: <?php echo $item ?></h4>
+            <?php endforeach ?>
+          </div>  
       <?php endforeach ?>
       </div>
     </div>
+    <?php endif ?>
   </div>
 </body>
 </html>
